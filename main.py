c275a049bbb772e1ab1518af74e29a8f983b2e0d
Alexis Guzman <spartanthelexvro@gmail.com>
2025-06-17 19:33:07 -0600

    Optimización suguerida

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 from ultralytics import YOLO
 import cv2
 
-# Cargar el modelo
+# 1. Modelo más ligero
 model = YOLO("models/best.pt")
+model.fuse()  # fusiona BatchNorm+Conv
 
-# Capturar camara
 cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+frame_id = 0
+skip = 2
 
 while cam.isOpened():
     ret, frame = cam.read()
-    if not ret:
-        break
+    if not ret: break
 
-    # Predicción del modelo
-    results = model(frame)
+    if frame_id % skip == 0:
+        # inferencia asíncrona (stream=True)
+        for results in model.track(frame, stream=True):
+            annotated = results.plot()
+            break
+    frame_id += 1
 
-    # Se anexan los resultados al frame mostrado y se visualiza
-    annotated_frame = results[0].plot()
-    cv2.imshow("YOLO Inferece", annotated_frame)
-
-    # Codición de salida
+    cv2.imshow("Fast YOLOv8", annotated)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
